372d2bdd49cf5dbb3d87700a43500512d7f39db2
Mark Latham <mark@votermedia.org>
2020-05-24 19:51:25 -0700

    digitalocean.com/community/tutorials/how-to-set-up-a-ruby-on-rails-project-with-a-react-frontend#step-5-—-configuring-react-as-your-rails-frontend

---
+++ app/javascript/packs/Index.jsx
@@ -0,0 +1,17 @@
+// Run this by adding <%= javascript_pack_tag 'Index' %> to the head of your layout file,
+// like app/views/layouts/application.html.erb.
+
+import React from "react";
+import { render } from "react-dom";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import $ from 'jquery';
+import Popper from 'popper.js';
+import 'bootstrap/dist/js/bootstrap.bundle.min';
+import App from "../components/App";
+
+document.addEventListener("DOMContentLoaded", () => {
+  render(
+    <App />,
+    document.body.appendChild(document.createElement("div"))
+  );
+});
